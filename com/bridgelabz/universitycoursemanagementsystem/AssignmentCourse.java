7ba01e8213d3c9b57436c6ffe8ddf252cf01403f
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 14:12:36 +0530

    [Sumit] feat: Implement AssignmentCourse class extending CourseType for assignment-based evaluations.

---
+++ com/bridgelabz/universitycoursemanagementsystem/AssignmentCourse.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.universitycoursemanagementsystem;
+
+class AssignmentCourse extends CourseType {
+    public AssignmentCourse() {
+        super("Assignment-Based Course");
+    }
+}
