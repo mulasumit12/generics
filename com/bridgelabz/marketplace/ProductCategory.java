e3951a75610f4975eadb2b7bd89ddbfe63dbe252
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 14:00:09 +0530

    [Sumit] feat: Add abstract ProductCategory class to define product categories.

---
+++ com/bridgelabz/marketplace/ProductCategory.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.marketplace;
+
+abstract class ProductCategory {
+    private String categoryName;
+
+    public ProductCategory(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
