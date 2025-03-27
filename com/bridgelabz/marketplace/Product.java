db5b2e59db14ab319818e394f0fe98def397245f
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 14:02:12 +0530

    [Sumit] feat: Create generic Product<T> class with bounded type parameter for various product categories.

---
+++ com/bridgelabz/marketplace/Product.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.marketplace;
+
+class Product<T extends ProductCategory> {
+    private String name;
+    private double price;
+    private T category;
+
+    public Product(String name, double price, T category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public T getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{Name='" + name + "', Price=" + price + ", Category='" + category.getCategoryName() + "'}";
+    }
+}
