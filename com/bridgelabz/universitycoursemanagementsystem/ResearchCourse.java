7d5524abefe1f9e102fdc256df4b1e3551c75e0f
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 14:13:24 +0530

    [Sumit] feat: Implement ResearchCourse class extending CourseType for research-based evaluations.

---
+++ com/bridgelabz/universitycoursemanagementsystem/ResearchCourse.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.universitycoursemanagementsystem;
+
+class ResearchCourse extends CourseType {
+    public ResearchCourse() {
+        super("Research-Based Course");
+    }
+}
