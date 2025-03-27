ef3fdead4254a366cc7085ba2bc59cdd59508886
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 13:42:57 +0530

    [Sumit] feat: Add generic Storage<T> class to handle warehouse items with add, remove, and retrieve functionalities.

---
+++ com/bridgelabz/warehouse/Storage.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.warehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
