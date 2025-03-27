8d60c904ffff8d22aba12e2176700c66c0f2f6ee
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 14:31:40 +0530

    [Sumit] feat: Add abstract JobRole class to define different job roles.

---
+++ com/bridgelabz/aidrivenresumeScreeningSystem/JobRole.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.aidrivenresumeScreeningSystem;
+
+abstract class JobRole {
+    private String roleName;
+
+    public JobRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
