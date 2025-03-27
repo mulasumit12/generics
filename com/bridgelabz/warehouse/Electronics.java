dabb93d6c7696ffbae15fab03749330b2dc22aac
mulasumit12 <mulasumit2019@gmail.com>
2025-03-27 13:41:51 +0530

    [Sumit] feat: Implement Electronics class extending WarehouseItem to represent electronic items in the warehouse

---
+++ com/bridgelabz/warehouse/Electronics.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.warehouse;
+
+class Electronics extends WarehouseItem {
+    public Electronics(String name, double price) {
+        super(name, price);
+    }
+}
+
+class Groceries extends WarehouseItem {
+    public Groceries(String name, double price) {
+        super(name, price);
+    }
+}
+
+class Furniture extends WarehouseItem {
+    public Furniture(String name, double price) {
+        super(name, price);
+    }
+}
